6d33fecf403ca17a6ee85613d7003259eb164284
Geoffrey Hanson <g.hanson.b@gmail.com>
2018-11-27 03:42:58 -0600

    fixed image issue

--- src/index.js
+++ src/index.js
@@ -5,13 +5,7 @@ import friends from './friends.json'
 
 function Card(props) {
   return (
-    // No need for arrow function, because `this` is not needed in a function component
-    // <button className="card" onClick={props.onClick}> 
-    //   <img alt={props.name} src={props.image} />
-    // </button>
-
-    // This actually works with the images.
-    <img className='card' onClick={props.onClick}  alt={props.name} src={props.image} />
+    <img className='card' onClick={props.onClick} alt={props.name} src={props.image} />
   );
 }
 
@@ -28,81 +22,75 @@ class Board extends React.Component {
   
   handleClick(i) {
     const cards = this.state.cards.slice(); // .slice() to make a copy for immutability
-    // ???
+
     if (endGame(cards) || cards[i]) { // If the game is over or clicking on an already filled card.
-    // if (endGame(cards)) {
-      // If resetting the score, is return needed?
       return this.setState({
         cards: Array(4).fill(null),
         score: this.state.score = 0, // eslint-disable-line
       });
-
-      // return this.setState({
-      //   cards: cards, // Refreshes the cards
-      //   score: this.state.score = 0,
-      // });
     }
-
-    // else if (cards[i]) {
-    //   return this.setState({
-    //     cards: Array(4).fill(null),
-    //     score: this.state.score = 0,
-    //   });
-    // }
-    
-    // If state is won, will not fill again.
-    // cards[i] = this.state.won ? null : 'X'; // Fill an X on cards[i], prevents filling after
 
     cards[i] = 'X';
 
-    this.setState({
-      // this is where randomization should go
+    return this.setState({
       cards: cards,
       score: this.state.score + 1,
-      // xIsNext: !this.state.xIsNext,
     });
   }
   
   renderCard(i) {
     return (
-      // <Card 
-      // value={this.state.cards[i]} 
-      // onClick={() => this.handleClick(i)}
-      // />
-      
-      
-        <Card 
-          value={this.state.cards[i]} 
-          onClick={() => this.handleClick(i)}
-          image={friends[i].image}
-          name={friends[i].name}
-        />
+      <Card 
+        value={this.state.cards[i]} 
+        onClick={() => this.handleClick(i)}
+        image={friends[i].image}
+        name={friends[i].name}
+      />
     );
   }
 
+  randomize() {
+    let random = Math.floor((Math.random() * 3) + 1);
+    console.log(random);
+    return random;
+  }
+
   render() {
-    // const status = (this.state.won ? 'You win!' : this.state.score);
-    // const status = (this.state.xIsNext ? 'X' : 'O');
 
     // Handling winner
     const winner = endGame(this.state);
     let status;
-    if (winner) {
-      status = 'You win!';
-    } else {
-      status = 'Your score: ' + (this.state.score);
+    winner ? status = 'You win!' : status = 'Your score: ' + (this.state.score);
+    
+    // RANDOMIZATION!!!
+    let place0;
+    let place1;
+    let place2;
+    let place3;
+    if (this.state.score === 0) {
+      place0 = this.renderCard(0);
+      place1 = this.renderCard(1);
+      place2 = this.renderCard(2);
+      place3 = this.renderCard(3); 
+    } 
+    else if (this.state.score > 0) {
+      place0 = this.renderCard(this.randomize());
+      place1 = this.renderCard(this.randomize());
+      place2 = this.renderCard(this.randomize());
+      place3 = this.renderCard(this.randomize());
     }
 
+    // At start, place0 = this.renderCard(0) in order for normal positions
     return (
       <div>
         <div className='score'>{status}</div>
         <div className='board-row'>
-          {this.renderCard(0)}
-          {this.renderCard(1)}
+          {place0}
+          {place1}
         </div>
         <div className='board-row'>
-          {this.renderCard(2)}
-          {this.renderCard(3)}
+          {place2}
+          {place3}
         </div>
       </div>
     );
@@ -117,8 +105,6 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className='game-info'>
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
